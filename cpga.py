29ea18f0a32011446e5e8f10ee5e22c272f051e7
Subodh Vashistha <43090551+vvashu38@users.noreply.github.com>
2020-06-29 21:59:17 +0530

    Update cpga.py

--- cpga.py
+++ cpga.py
@@ -6,22 +6,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 username = 'Your_username'
 password = 'Your_password'
-
 url = 'https://mis.nita.ac.in'
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
 driver.get(url)
-#time.sleep(3) # decrease or increase as per your convenience
+
 driver.find_element_by_id('txt_username').send_keys(username)
 driver.find_element_by_id('txt_password').send_keys(password)
-
 captcha = str(input("Enter captcha"))
 driver.find_element_by_id('txt_captchalgn').send_keys(captcha)
 time.sleep(0.001)
 driver.find_element_by_id('btnSubmit').click()
 driver.maximize_window()
-
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"Div6\"]/div/div[2]/div/script")))
 
@@ -31,7 +26,6 @@ i = len(result)
 a = 0
 for x in result:
 	a+=x
-
 a = a/i
 
 driver.execute_script('alert("Your cgpa is {:.2f} ")'.format(round(a,2)))
